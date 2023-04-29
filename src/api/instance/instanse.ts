8317de36f77ca74ca5bd5047a66e7388f1d026f5
admin <krivalcevich@gmail.com>
2023-04-29 17:50:01 +0300

    the end

--- src/api/instance/instanse.ts
+++ src/api/instance/instanse.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = "http://engine.hotellook.com/api/v2/cache.json";
+export const API_URL = "https://engine.hotellook.com/api/v2/cache.json";
 
 export const instance = axios.create({
     baseURL: API_URL,
